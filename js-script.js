f4f238a0f1045559ba29a9d3b84bb9e162e3c99e
Gabriele Bighiu <gabrielebighiu2@gmail.com>
2022-09-14 17:38:45 +0100

    Completed files

---
+++ js-script.js
@@ -0,0 +1,33 @@
+let words = [];
+let displayText = document.getElementById("displayText");
+
+// Adds the word from the 'Add' section and stores in the words[] array
+function addWord() {
+    if (findWord(document.getElementById("addWordInput").value.toUpperCase())) {
+        displayText.innerHTML = "This word has already been added to the ditionary";
+        document.getElementById("display").appendChild(displayText);
+        //window.alert(1);
+    } else {
+        words[words.length] = document.getElementById("addWordInput").value.toUpperCase();
+        displayText.innerHTML = "This word has been added to the dictionary";
+        document.getElementById("display").appendChild(displayText);
+        //window.alert(2);
+    }
+}
+
+// Looks for a word in words[] array. indexOf() returns a positive number if word is in words[], or else it returns -1
+function findWord(word) {
+    if (words.indexOf(word) > -1) {
+        return true;
+    }
+    return false;
+}
+
+
+function checkWord() {
+    if (findWord(document.getElementById("checkWordInput").value.toUpperCase())) {
+        displayText.innerHTML = "The searched word is in the dictionary";
+    } else {
+        displayText.innerHTML = "The searched word is not in the dictionary";
+    }
+}
